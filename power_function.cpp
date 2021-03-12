5e777df1f68d2b3b86448b4e7305c52f64f95122
Rajan Kumar <rajanrajshantoshi8651@gmail.com>
2021-03-12 19:25:44 +0530

    unction power() - raise a number m to a power n
     Write a function power() which raise a number m to a power n. The function takes double value of m and integer value of n and returns the result. Use the default value of n as 2.

---
+++ power_function.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<ostream>
+#include<cmath>
+
+using namespace std;
+
+double power(double m,int n);
+int main()
+{
+   int n=2;
+   double raised ,m;
+cout<<"\nEnter the value of m:";
+cin>>m;
+cout<<"\nEnter the value of n:";
+cin>>n;
+
+raised=power(m,n);
+cout<<"m raised to power n is="<<raised <<endl;
+
+}
+double power(double m, int n)
+{
+  double x;
+
+ x=pow(m,n);
+
+return x ;
+
+}
